decf24b2dbbe51d34a93f336420b0b1f8c762177
Khofifah Mega <khofifahmgm@gmail.com>
2021-01-09 10:09:57 +0700

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:belajar_flutter/utils/app_sizes.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          'Kelas Mobile Malang',
+          style: TextStyle(
+            fontSize: FontSize.s30,
+            fontFamily: 'BlackCasper',
+          ),
+        ),
+      ),
+    );
+  }
+}
